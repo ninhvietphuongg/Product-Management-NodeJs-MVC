f852c4a552746c4de401d69ff2934ab540c22cd8
Ninh Viet Phuong <hotro.phuongsieunhan@gmail.com>
2024-06-02 21:47:48 +0700

    display-data'products

--- controller/admin/product.controller.js
+++ controller/admin/product.controller.js
@@ -1,5 +1,8 @@
-module.exports.index = (req, res) => {
+const Product = require("../../model/system");
+module.exports.index = async (req, res) => {
+    const products = await Product.find({})
     res.render("admin/products/index", {
-        pageTitle: "Trang danh sách sản phẩm"
+        pageTitle: "Trang danh sách sản phẩm",
+        products : products
     })
 }
