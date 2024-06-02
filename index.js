25aa7758cf3e0aca7f23ba9fabf3d9328cfcc923
Ninh Viet Phuong <hotro.phuongsieunhan@gmail.com>
2024-06-02 14:57:57 +0700

    setting-projecct

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Hello world");
+const express = require("express");
+const app = express();
+const port = 3000;
+app.get("/", (req, res) => {
+    res.send("Hello world");
+})
+app.listen(port, () => {
+    console.log(`Listening to ${port}`);
+})
