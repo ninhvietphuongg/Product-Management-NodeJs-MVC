5d6a85b7e046158c360e0e1a0da8a315c3d66613
Ninh Viet Phuong <hotro.phuongsieunhan@gmail.com>
2024-06-03 16:06:01 +0700

    optimal-code-filterStatus

--- controller/admin/product.controller.js
+++ controller/admin/product.controller.js
@@ -1,31 +1,15 @@
 const Product = require("../../model/system");
+const helpersFilterStatus = require("../../helpers/filter.status");
 module.exports.index = async (req, res) => {
     const find = {
         deleted : false,
     }
-    const filterStatus = [{
-        name : 'Tất cả',
-        status : "",
-        class : ""
-    },{
-        name : "Hoạt động",
-        status : "active",
-        class : ""
-    },{
-        name : "Dừng hoạt động",
-        status : "inactive",
-        class : ""
-    }]
-    if(req.query.status){
-        const index = filterStatus.findIndex(item => item.status == req.query.status);
-        filterStatus[index].class = "active";
-    }else{
-        const index = filterStatus.findIndex(item => item.status == "");
-        filterStatus[index].class = "active";
-    }
+    // Filter status
+    const filterStatus = helpersFilterStatus(req);
     if(req.query.status){
         find.status = req.query.status;
     }
+    // End filter status
     const products = await Product.find(find)
     res.render("admin/products/index", {
         pageTitle: "Trang danh sách sản phẩm",
