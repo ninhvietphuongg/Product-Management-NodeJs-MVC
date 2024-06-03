53f64eb777f8d75fe2d2cccd0254eb1cef6d6a77
Ninh Viet Phuong <hotro.phuongsieunhan@gmail.com>
2024-06-03 16:39:55 +0700

    search-value-for-admin

--- controller/admin/product.controller.js
+++ controller/admin/product.controller.js
@@ -10,6 +10,12 @@ module.exports.index = async (req, res) => {
         find.status = req.query.status;
     }
     // End filter status
+    // Find Search Title
+    if(req.query.keyword){
+        const regex = new RegExp(req.query.keyword,"i");
+        find.title = regex;
+    }
+    // End Find Search Title
     const products = await Product.find(find)
     res.render("admin/products/index", {
         pageTitle: "Trang danh sách sản phẩm",
--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -1,3 +1,4 @@
+// List Button Status
 const listButtonStatus = document.querySelectorAll("[button-status]");
 if(listButtonStatus.length > 0){
     let url = new URL(window.location.href)
@@ -14,3 +15,22 @@ if(listButtonStatus.length > 0){
         })
     })
 }
+// End List Button Status
+// Form Search Keyword
+const formSearch = document.querySelector("#form-search");
+if(formSearch){
+    let url = new URL(window.location.href);
+    const buttonFormSearch = formSearch.querySelector("[button-form-search]");
+    formSearch.addEventListener("submit", (event) => {
+        event.preventDefault();
+        const keyword = event.target.elements.keyword.value;
+        if(keyword){
+        url.searchParams.set("keyword", keyword);
+        }else{
+        url.searchParams.delete("keyword");
+        }
+        window.location.href = url.href; 
+
+    })
+}
+// End Form Search Keyword
