16b5e4cce368a7bc625ed2ff650c894b593f6ffc
Ninh Viet Phuong <hotro.phuongsieunhan@gmail.com>
2024-06-03 15:54:36 +0700

    filter-status

--- controller/admin/product.controller.js
+++ controller/admin/product.controller.js
@@ -3,7 +3,22 @@ module.exports.index = async (req, res) => {
     const find = {
         deleted : false,
     }
-  
+    const filterStatus = [{
+        name : 'Tất cả',
+        status : "",
+        class : ""
+    },{
+        name : "Hoạt động",
+        status : "active",
+        class : ""
+    },{
+        name : "Dừng hoạt động",
+        status : "inactive",
+        class : ""
+    }]
+    if(req.query.status){
+        find.status = req.query.status;
+    }
     const products = await Product.find(find)
     res.render("admin/products/index", {
         pageTitle: "Trang danh sách sản phẩm",
--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -1,9 +1,16 @@
 const listButtonStatus = document.querySelectorAll("[button-status]");
 if(listButtonStatus.length > 0){
+    let url = new URL(window.location.href)
     listButtonStatus.forEach(button => {
         button.addEventListener("click", () => {
             const status = button.getAttribute("button-status");
-            console.log(status);
+            if(status){
+            url.searchParams.set("status", status)
+            window.location.href = url.href
+            }else{
+            url.searchParams.delete("status")
+            window.location.href = url.href
+            }
         })
     })
 }
