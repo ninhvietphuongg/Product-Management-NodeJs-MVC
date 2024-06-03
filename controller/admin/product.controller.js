30b19ba580e250cb79deeddacdb1e67abebc2e67
Ninh Viet Phuong <hotro.phuongsieunhan@gmail.com>
2024-06-03 15:57:46 +0700

    add-class-filter-status

--- controller/admin/product.controller.js
+++ controller/admin/product.controller.js
@@ -17,6 +17,13 @@ module.exports.index = async (req, res) => {
         class : ""
     }]
     if(req.query.status){
+        const index = filterStatus.findIndex(item => item.status == req.query.status);
+        filterStatus[index].class = "active";
+    }else{
+        const index = filterStatus.findIndex(item => item.status == "");
+        filterStatus[index].class = "active";
+    }
+    if(req.query.status){
         find.status = req.query.status;
     }
     const products = await Product.find(find)
