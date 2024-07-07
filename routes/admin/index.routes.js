79d02c1c8c77285087fc115a82ef8673d5acfe1e
Ninh Viet Phuong <hotro.phuongsieunhan@gmail.com>
2024-07-07 13:08:37 +0700

    setting local variable admin for admin

--- routes/admin/index.routes.js
+++ routes/admin/index.routes.js
@@ -1,6 +1,8 @@
 const dashboardAdmin = require("./dashboard.routes");
 const productsAdmin = require("./product.routes");
+const systemConfix = require("../../config/system.confix");
 module.exports = (app) => {
-    app.use("/admin", dashboardAdmin);
-    app.use("/admin", productsAdmin);
+    const PathAdmin = `/${systemConfix.prefixAdmin}`;
+    app.use(PathAdmin, dashboardAdmin);
+    app.use(PathAdmin, productsAdmin);
 }
