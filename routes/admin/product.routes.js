f9a5040637b5a0ec468d3aee12e43fd872dd638a
Ninh Viet Phuong <hotro.phuongsieunhan@gmail.com>
2024-07-08 17:11:03 +0700

    create dât don't have upload image

--- routes/admin/product.routes.js
+++ routes/admin/product.routes.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const multer  = require('multer')
+const upload = multer({ dest: './public/admin/img/' })
 const productsAdmin = require("../../controller/admin/product.controller");
 const router = express.Router();
 router.get("/products", productsAdmin.index);
 router.patch(`/products/change-status/:status/:id`, productsAdmin.changeStatus);
 router.delete(`/products/delete-status/:id`, productsAdmin.deleteStatus);
 router.patch(`/products/change-multi-status`, productsAdmin.changeMultiStatus);
+router.get("/products/create", productsAdmin.createIndex)
+router.post("/products/create", upload.single("thumbnail") ,productsAdmin.createPost)
 module.exports = router;
